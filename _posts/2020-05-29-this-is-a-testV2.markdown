ea99914d2ebbe8cacde039a1debb6e7d995bce05
BenLeistiko <38890737+BenLeistiko@users.noreply.github.com>
2020-05-29 16:52:35 -0700

    Add files via upload

---
+++ _posts/2020-05-29-this-is-a-testV2.markdown
@@ -0,0 +1,9 @@
+---
+layout: single
+title:  "This is a Test V3"
+date:   2020-05-29 16:18:33 -0700
+categories: jekyll update
+---
+
+
+This is a page that I made!
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/2020/05/29/this-is-a-test.html",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/2020/05/29/welcome-to-jekyll.html",
+        "teaser": null
+      },{
+        "title": "This is a Test V3",
+        "excerpt":"This is a page that I made!  ","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/2020/05/29/this-is-a-testV2.html",
+        "teaser": null
+      }]
